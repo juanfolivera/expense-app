033e5c310d63fc0ceaee155f6ea3770a31baf9df
Juan Olivera <juan.olivera@tarmac.io>
2026-04-07 13:37:42 -0300

    Initial Commit

---
+++ expense-app/expense_app.swift
@@ -0,0 +1,17 @@
+//
+//  expense_app.swift
+//  expense-app
+//
+//  Created by Juan Olivera on 7/4/26.
+//
+
+import SwiftUI
+
+@main
+struct expense_app: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ expense-appTests/expense_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  expense_appTests.swift
+//  expense-appTests
+//
+//  Created by Juan Olivera on 7/4/26.
+//
+
+import Testing
+@testable import expense_app
+
+struct expense_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
